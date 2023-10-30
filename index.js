4741d50b84bb47c010201fa42c7895d623aa31ca
Muydinov Mubosher <muydinovmubosher@yandex.ru>
2023-10-30 08:48:18 +0500

    websocket

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const WebSocket = require("ws");
 
 const app = express();
 
@@ -9,7 +10,6 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const PORT = process.env.PORT || 5000;
-
 
 app.get("/", (req, res) => {
   res.json({ message: "Developing this application" });
@@ -19,6 +19,30 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("DB connected");
-    app.listen(PORT, () => console.log(`Server has running : ${PORT}`));
+    
+    const server = app.listen(PORT, () =>
+      console.log(`Server has running : ${PORT}`)
+    );
+
+    // Create WebSocket server
+    const wss = new WebSocket.Server({ server });
+
+    // WebSocket connection handler
+    wss.on("connection", (ws) => {
+      console.log("WebSocket client connected");
+
+      // WebSocket message received
+      ws.on("message", (message) => {
+        console.log("Received message:", message);
+
+        // Echo the message back to the client
+        ws.send(`Echo: ${message}`);
+      });
+
+      // WebSocket connection closed
+      ws.on("close", () => {
+        console.log("WebSocket client disconnected");
+      });
+    });
   })
   .catch((err) => console.log(err));
